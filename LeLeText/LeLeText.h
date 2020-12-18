1703b5f2e83fa2868c65ca0454da60ecfc27bb2a
yc_htl <huangtianle@mbcloud.com>
2020-12-18 11:43:19 +0800

    initial commit

---
+++ LeLeText/LeLeText.h
@@ -0,0 +1,18 @@
+//
+//  LeLeText.h
+//  LeLeText
+//
+//  Created by  yc_htl on 2020/12/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LeLeText.
+FOUNDATION_EXPORT double LeLeTextVersionNumber;
+
+//! Project version string for LeLeText.
+FOUNDATION_EXPORT const unsigned char LeLeTextVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeLeText/PublicHeader.h>
+
+
---
+++ LeLeText/TsetTool.swift
@@ -0,0 +1,18 @@
+//
+//  TsetTool.swift
+//  LeLeText
+//
+//  Created by  yc_htl on 2020/12/18.
+//
+
+import UIKit
+
+open class TsetTool: NSObject {
+    public func sayHello() {
+        print("hello")
+    }
+    
+    public func sayHelloSwift() {
+        print("helloSwift")
+    }
+}
